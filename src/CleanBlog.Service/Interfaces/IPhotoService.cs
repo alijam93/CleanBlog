f10a98caa01786170a9b979b3533923db3e30286
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-06-27 20:15:40 +0430

    add service interfaces

--- src/CleanBlog.Service/Interfaces/IPhotoService.cs
+++ src/CleanBlog.Service/Interfaces/IPhotoService.cs
@@ -1,14 +1,16 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using CleanBlog.Shared.Features.File;
 
-//namespace CleanBlog.Service.Interfaces
-//{
-//    public interface IPhotoRepository
-//    {
-//        string AddImagePost(UploadModel file);
-//        string AddAvatar(UploadModel file);
-//    }
-//}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanBlog.Service.Interfaces
+{
+    public interface IPhotoRepository
+    {
+        string AddImagePost(UploadModel file);
+        string AddAvatar(UploadModel file);
+    }
+}
--- src/CleanBlog.Service/Interfaces/IPostService.cs
+++ src/CleanBlog.Service/Interfaces/IPostService.cs
@@ -1,27 +1,23 @@
-﻿//using Blog.Shared.DTOs;
-//using Blog.Shared.Models;
-//using Blog.Shared.Providers.File;
-//using Blog.Shared.Providers.Pagination;
-//using Blog.Storage.Utils;
+﻿using CleanBlog.Shared.Dtos;
+using CleanBlog.Shared.Features.File;
+using CleanBlog.Shared.Features.Pagination;
 
-//using CleanBlog.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-
-//namespace CleanBlog.Service.Interfaces
-//{
-//    public interface IPostService
-//    {
-//        Task<PagedList<PostDTO>> GetPosts(PostParameters postParameters);
-//        Task<PagedList<PostDTO>> GetPostsByTag(PostParameters postParameters, string name);
-//        Task<PostDTO> GetPostById(int id);
-//        Task AddPost(AddPostDTO postDTO);
-//        Task UpdatePost(EditPostDTO postDTO);
-//        Task DeletePost(int id);
-//        string SendImage(UploadModel image);    
-//        bool PostItemExists(int id);
-//    }
-//}
+namespace CleanBlog.Service.Interfaces
+{
+    public interface IPostService
+    {
+        Task<PagedList<PostDTO>> GetPosts(PostParameters postParameters);
+        Task<PagedList<PostDTO>> GetPostsByTag(PostParameters postParameters, string name);
+        Task<PostDTO> GetPostById(int id);
+        Task AddPost(AddPostDTO postDTO);
+        Task UpdatePost(EditPostDTO postDTO);
+        Task DeletePost(int id);
+        string SendImage(UploadModel image);
+        bool PostItemExists(int id);
+    }
+}
