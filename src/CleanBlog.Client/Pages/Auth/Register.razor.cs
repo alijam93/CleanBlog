cd52a450a1d7e0c49e5d6009913279af0b1bfe4f
ajam93 <52130390+ajam93@users.noreply.github.com>
2021-07-19 21:36:01 +0430

    update auth client page

---
+++ src/CleanBlog.Client/Pages/Auth/Register.razor.cs
@@ -0,0 +1,102 @@
+﻿using CleanBlog.Client.Utils.Models;
+using CleanBlog.Client.Utils.Statics;
+using CleanBlog.Shared.Features.File;
+
+using Microsoft.AspNetCore.Components.Forms;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace CleanBlog.Client.Pages.Auth
+{
+    public partial class Register
+    {
+        protected RegisterVM RegisterModel = new();
+        private bool ShowErrors = false;
+        private IEnumerable<string> Errors;
+
+        public string ImgUrl { get; set; }
+        private EditContext editContext;
+        public MultipartFormDataContent content = new();
+
+        string submitButton = "btn btn-primary";
+        bool disable = true;
+
+        protected override void OnInitialized()
+        {
+            editContext = new EditContext(RegisterModel);
+        }
+
+        private async Task HandleRegistration()
+        {
+            DisableButton(true);
+
+            if (RegisterModel.Picture != null)
+            {
+                var resultUpload = await http.PostAsync(Endpoints.Photo + "addAvatar", content);
+                var uploadAddress = await resultUpload.Content.ReadAsStringAsync();
+                if (resultUpload.IsSuccessStatusCode)
+                {
+                    RegisterModel.Avatar = uploadAddress.Trim('"');
+                    disable = false;
+                }
+            }
+
+
+            var result = await AuthService.Register(RegisterModel);
+
+            if (result.Successful)
+            {
+                navigation.NavigateTo("/");
+            }
+            else
+            {
+                await http.PostAsJsonAsync(Endpoints.Photo + "deleteFile", new DeleteFile { Path = RegisterModel.Avatar });
+                Errors = result.Errors;
+                ShowErrors = true;
+                DisableButton(false);
+            }
+        }
+
+        async Task OnChangeImage(InputFileChangeEventArgs e)
+        {
+            RegisterModel.Picture = e.File;
+            RegisterModel.FileName = e.File.Name;
+            editContext.NotifyFieldChanged(FieldIdentifier.Create(() => RegisterModel.Picture));
+            var format = "image/jpeg";
+            var resizedFile = await e.File.RequestImageFileAsync(format, 300, 200);
+            using var ms = resizedFile.OpenReadStream(resizedFile.Size);
+            await InvokeAsync(StateHasChanged);
+
+            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data");
+            content.Add(new StreamContent(ms, Convert.ToInt32(resizedFile.Size)), "Image", resizedFile.Name);
+            content.Add(new StringContent($"{RegisterModel.FileName}"), "Name");
+
+
+            using var memoryStream = new MemoryStream();
+            using var fileStream = resizedFile.OpenReadStream(1024 * 1024 * 15);
+
+            await fileStream.CopyToAsync(memoryStream);
+
+            ImgUrl = $"data:{format};base64,{Convert.ToBase64String(memoryStream.ToArray())}";
+        }
+
+        void DisableButton(bool status)
+        {
+            if (status == true)
+            {
+                submitButton = "btn btn-secondary disabled";
+            }
+            else
+            {
+                submitButton = "btn btn-primary";
+            }
+        }
+    }
+}
---
+++ src/CleanBlog.Client/Shared/Login/Login.razor.cs
@@ -0,0 +1,71 @@
+﻿using CleanBlog.Shared.Identity.Account;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanBlog.Client.Shared.Login
+{
+    public partial class Login
+    {
+        protected LoginModel loginModel = new();
+        private bool ShowErrors = false;
+
+        private string Error = "";
+
+        string submitButton = "";
+
+        private async Task HandleLogin()
+        {
+            DisableButton(true);
+
+            var result = await AuthService.Login(loginModel);
+
+            if (result.IsSuccessful)
+            {
+                Close();
+
+                DisableButton(false);
+            }
+            else
+            {
+                Error = result.ErrorMessage;
+                ShowErrors = true;
+                DisableButton(false);
+            }
+        }
+
+        public string ModalDisplay = "none;";
+        public string ModalClass = "";
+        public bool ShowBackdrop = false;
+
+        public void Open()
+        {
+            ModalDisplay = "block;";
+            ModalClass = "show";
+            ShowBackdrop = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            ModalDisplay = "none";
+            ModalClass = "";
+            ShowBackdrop = false;
+            StateHasChanged();
+        }
+
+        void DisableButton(bool status)
+        {
+            if (status == true)
+            {
+                submitButton = "button-disable";
+            }
+            else
+            {
+                submitButton = "";
+            }
+        }
+    }
+}
